b280e4668706354e322ccc1fae88045e10e145a4
Andrei Kaliachka <andrei_kaliachka@epam.com>
2023-01-19 05:27:09 -0800

    Init spring boot project

---
+++ src/main/java/org/rement/localstackservice/LocalstackServiceApplication.java
@@ -0,0 +1,13 @@
+package org.rement.localstackservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalstackServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalstackServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/rement/localstackservice/LocalstackServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rement.localstackservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalstackServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
